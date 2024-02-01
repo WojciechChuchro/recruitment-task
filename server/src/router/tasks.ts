ab17ba94b63d221069a197ee7b8092dc71adcdfe
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2024-02-01 19:16:50 +0100

    Feat: implement basic routes

--- server/src/router/tasks.ts
+++ server/src/router/tasks.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import {createTask, getAllTasks, getTask} from "../controllers/tasks";
+import {validateAdministrator, validateCreator, validateJWT} from "../controllers/authentication";
+import {authenticateJWT} from "../middlewares";
 
 
 export default (router: express.Router) => {
-
+    router.post('/task', authenticateJWT, createTask);
+    router.get('/task/:id', authenticateJWT, validateCreator, getTask);
+    router.get('/task',authenticateJWT, validateAdministrator, getAllTasks)
 };
