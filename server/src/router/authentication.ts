53e92e7f00d142fa78bdc1d8bfa6497a3d9ac4c2
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2024-02-01 17:52:00 +0100

    Feat: create auth router

---
+++ server/src/router/authentication.ts
@@ -0,0 +1,14 @@
+import {
+  login,
+  logout,
+  register,
+  validateJWT,
+} from '../controllers/authentication';
+import express from 'express';
+
+export default (router: express.Router) => {
+  router.post('/auth/register', register);
+  router.post('/auth/login', login);
+  router.get('/auth/logout', logout);
+  router.get('/auth/validate-jwt', validateJWT);
+};
