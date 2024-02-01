603eb80bfe498752978eea36f9d1ecdc9e913cce
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2024-02-01 19:13:45 +0100

    Add: creatorId field

--- server/src/database/models/tasks.model.ts
+++ server/src/database/models/tasks.model.ts
@@ -1,16 +1,24 @@
-import { Model } from 'objection';
+import {Model} from 'objection';
 import knex from '../config/database';
 import Users from "./users.model";
+import {Task} from "../../controllers/tasks";
 
 Model.knex(knex);
 
-class Tasks extends Model {
+class Tasks extends Model implements Task {
+    id: number;
+    userId: number;
+    creatorId: number;
+    isDone: number;
+    description: string;
+
     static get Streets() {
         return {
-            required: ['id', 'userId', 'isDone', 'description'],
+            required: ['id', 'userId', 'isDone', 'description', 'creatorId'],
             properties: {
                 id: { type: 'integer' },
                 userId: { type: 'integer' },
+                creatorId: { type: 'integer' },
                 isDone: {type: 'boolean' },
                 description: { type: 'string', length: 50 },
             },
