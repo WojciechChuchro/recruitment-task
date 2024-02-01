8ea84138a7271f7f094f740d07b041a9da8df3ce
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2024-02-01 19:13:09 +0100

    Update migration by creatorId field

--- server/src/database/migrations/2_tasks.ts
+++ server/src/database/migrations/2_tasks.ts
@@ -4,9 +4,11 @@ export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('tasks', (table: Knex.TableBuilder) => {
         table.increments('id').unsigned().primary().notNullable();
         table.integer('userId').unsigned().notNullable();
+        table.integer('creatorId').unsigned().notNullable();
         table.boolean('isDone').notNullable();
         table.string('description', 50).notNullable();
         table.foreign('userId').references('id').inTable('users');
+        table.foreign('creatorId').references('id').inTable('users');
     });
 }
 
