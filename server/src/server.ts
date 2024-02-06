420c6fa057fe7562ef3cfef4f40b8fe642bdcf56
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2024-02-06 16:15:32 +0100

    Add cors for front-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 import mysql from "mysql2";
 import router from "./router";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 export const dbConfig = {
   host: 'localhost',
   user: process.env.DB_USER || 'root',
@@ -15,6 +15,12 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+}));
 
 app.listen(process.env.PORT || 8081, () => {
   console.log(
